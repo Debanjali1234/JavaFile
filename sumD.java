9d34b2e255b424c86e23d532031c86551ba8196e
DEBANJALI BHATTACHARYA <126330453+Debanjali1234@users.noreply.github.com>
2024-02-07 21:44:33 +0530

    Add files via upload

---
+++ sumD.java
@@ -0,0 +1,12 @@
+
+public class sumD {
+    public static void main(String args[]) {
+        float x, y;
+        float sum;
+        x = 999999.9999f;
+        y = 1074924.794847f;
+        sum = (x + y);
+        System.out.println("The Sum of " + x + " and " + y + " is " + sum);
+
+    }
+}
