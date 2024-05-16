adee8b493591253783647918f703320749b9dab3
DEBANJALI BHATTACHARYA <126330453+Debanjali1234@users.noreply.github.com>
2024-05-16 18:22:15 +0530

    Add files via upload

---
+++ code14.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class code14 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        String R = new StringBuilder(A).reverse().toString();
+        if (A.equals(R)) {
+            System.out.println("Yes");
+        } else
+            System.out.println("No");
+
+    }
+}
