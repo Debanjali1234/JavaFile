5564f7d465321b50be975cab24ce9823105328c0
DEBANJALI BHATTACHARYA <126330453+Debanjali1234@users.noreply.github.com>
2024-07-22 02:16:17 +0530

    Add files via upload

---
+++ SOA.java
@@ -0,0 +1,17 @@
+public class SOA {
+    public static void main(String[] args) {
+        // Example array
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        // Variable to hold the sum
+        int sum = 0;
+
+        // Loop through the array to calculate the sum
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+
+        // Output the sum
+        System.out.println("The sum of the array is: " + sum);
+    }
+}
