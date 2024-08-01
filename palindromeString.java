6fcf9d50bfe46363d655e96b49f35ff3981eb840
DEBANJALI BHATTACHARYA <126330453+Debanjali1234@users.noreply.github.com>
2024-08-02 02:51:36 +0530

    Add files via upload

---
+++ palindromeString.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class palindromeString {
+
+    public static boolean palindrome(String str) {
+
+        for (int i = 0; i < str.length() / 2; i++) {
+
+            int n = str.length();
+
+            if (str.charAt(i) != str.charAt(n - 1 - i)) {
+
+                return false;
+            }
+
+        }
+        return true;
+
+    }
+
+    public static void main(String args[]) {
+
+        String str = "racecar";
+
+        System.out.println(palindrome(str));
+
+    }
+
+}
