fee27607e9c32bb36284456189d56189dfb5ceb4
DEBANJALI BHATTACHARYA <126330453+Debanjali1234@users.noreply.github.com>
2024-08-09 14:06:11 +0530

    Add files via upload

---
+++ stringcompressionj.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class stringcompressionj {
+
+    public static String STRINGCOMPRESSION(String str) {
+
+        String newstr = " ";
+        for (int i = 0; i < str.length(); i++) {
+
+            Integer count = 1;
+            while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+            newstr += str.charAt(i);
+            if (count > 1) {
+                newstr += count.toString();
+            }
+        }
+        return newstr;
+
+    }
+
+    public static void main(String[] args) {
+        String str = "aaaaaaaabbbbbbcdeeeeeeeefffffffffffjjjjjjjjjjjjjjkkkkkkkkkkkkkkkkkp";
+
+        System.out.println(STRINGCOMPRESSION(str));
+    }
+
+}
